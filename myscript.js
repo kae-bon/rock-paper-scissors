16dce5a6505b8f808648befbb81dc3b025bd0a84
kae-bon <k.n.bonaguro@gmail.com>
2021-10-25 19:43:28 -0400

    Finished up the JS! Wrote a single round function inside of a game function, and then called that game function 5 times using a for loop. Added in a score function as well and logged it to the console so you can see the score at the end.

--- myscript.js
+++ myscript.js
@@ -4,43 +4,72 @@ and then used the Math.floor and Math.random
 methods to pick a random object from that array. 
 The random object is returned and then logged. */
 
-function computerPlay() {
-    let computerChoice = ['rock', 'paper', 'scissors'];
-    let randomChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    return randomChoice;
-}
-console.log(computerPlay());
-console.log(typeof computerPlay);
+let computerScore = 0;
+let playerScore = 0;
 
-function oneGame() {
-let playerSelection = window.prompt('You\'re playing Rock, Paper, Scissors! Choose your fighter!');
-playerSelection = playerSelection.toLowerCase();
-return playerSelection;
-}
-console.log(oneGame());
+console.log(playerScore);
+console.log(computerScore);
 
-/*
-function singleGame() {
-    if (playerSelection.toLowerCase ===  "rock" && computerPlay() === "paper")
-}
-console.log(playerSelection);
-/*
-function singleRound(playerSelection, computerSelection) {
+function computerChoice() {
+    let choiceOptions = ['rock', 'paper', 'scissors'];
+    return choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
 }
 
-*/ 
+function playerChoice() {
+let playerInput = window.prompt('You\'re playing Rock, Paper, Scissors! Choose your fighter!');
+return playerInput.toLowerCase();
+}
 
-/* Write a function that plays a single round of RPS. 
-The function should take two parameters (playerSelection
-    and computerSelection) and return a string that declares
-    the winner of the round like so:"You lose! Paper beats rock." 
-    
-To-Do:
-1. Create variable for playerSelection and assign.
-2. Create variable for computerSelection and assign.
-3. Compare playerSelection to computerSelection using and if...else function.
-Rock > Scissors
-Scissors > Paper
-Paper > Rock
+function game() {
+        function playRound(playerSelection, computerSelection) {
+            computerSelection = computerChoice();
+            playerSelection = playerChoice();
+        if (playerSelection === 'rock' && computerSelection === 'paper') {
+            computerScore++;
+            return 'Paper beats rock! You lose!' + '\n Computer Score: ' + computerScore
+            + '\n Player Score: ' + playerScore;
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            playerScore++;
+            return 'Paper beats rock! You win!' + '\n Computer Score: ' + computerScore
+            + '\n Player Score: ' + playerScore;
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            computerScore++;
+            return 'Scissors beat paper! You lose!' + '\n Computer Score: ' + computerScore
+            + '\n Player Score: ' + playerScore;
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            playerScore++;
+            return 'Paper beats rock! You win!' + '\n Computer Score: ' + computerScore
+            + '\n Player Score: ' + playerScore;;
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            playerScore++;
+            return 'Scissors beat paper! You win!' + '\n Computer Score: ' + computerScore
+            + '\n Player Score: ' + playerScore;;
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+            computerScore++;
+            return 'Rock beats scissors! You lose! ' + '\n Computer Score: ' + computerScore
+            + '\n Player Score: ' + playerScore;
+        } else (playerSelection === computerSelection) 
+            return 'It\'s a tie!' + '\n Computer Score: ' + computerScore
+            + '\n Player Score: ' + playerScore;
+        }
+    return playRound();
+}
 
-    */
+for (i = 1; i <=5; i++) {
+    console.log(game());
+}
+
+function score() {
+    if (playerScore > computerScore) {
+        return 'You won!!!!!!!' + '\n Computer Score: ' + computerScore
+        + '\n Player Score: ' + playerScore;
+    } else if (playerScore == computerScore) {
+        return 'It\'s a tie!' + '\n Computer Score: ' + computerScore
+        + '\n Player Score: ' + playerScore;
+    } else {
+        return 'You lost!!!!!!!!' + '\n Computer Score: ' + computerScore
+        + '\n Player Score: ' + playerScore;
+    }
+}
+
+console.log(score());
