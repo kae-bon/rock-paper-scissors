1fe2f501a3f0f124a0b152f1675016a57676995f
kae-bon <k.n.bonaguro@gmail.com>
2021-11-27 20:00:47 -0500

    Cleaned up code. Added Event Listener to buttons to console.log button text content.

--- myscript.js
+++ myscript.js
@@ -1,75 +1,51 @@
-/* I started with the computerPlay function.
-I started by making an array of the choices, 
-and then used the Math.floor and Math.random
-methods to pick a random object from that array. 
-The random object is returned and then logged. */
+const btns = document.querySelectorAll('button');
+btns.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.textContent);
+        playRound();
+    });
+}
+);
 
-let computerScore = 0;
 let playerScore = 0;
+let computerScore = 0;
 
-console.log(playerScore);
-console.log(computerScore);
+function playRound(computer, player) {
+    computer = computerPlay();
+    player = playerPlay();
 
-function computerChoice() {
-    let choiceOptions = ['rock', 'paper', 'scissors'];
-    return choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
-}
+    function computerPlay() {
+        let computerChoices = ['rock', 'paper', 'scissors'];
+        let computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        return computerSelection;
+    }
 
-function playerChoice() {
-let playerInput = window.prompt('You\'re playing Rock, Paper, Scissors! Choose your fighter!');
-return playerInput.toLowerCase();
-}
-
-function game() {
-        function playRound(playerSelection, computerSelection) {
-            computerSelection = computerChoice();
-            playerSelection = playerChoice();
-        if (playerSelection === 'rock' && computerSelection === 'paper') {
+    function playerPlay() {
+        if ((player === 'rock' && computer === 'paper') ||
+        (player === 'paper' && computer === 'scissors') ||
+        (player === 'scissors' && computer === 'rock')) {
             computerScore++;
-            return 'Paper beats rock! You lose!' + '\n Computer Score: ' + computerScore
-            + '\n Player Score: ' + playerScore;
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            return '`${computerFinal} beats ${playerFinal}`! You lose!';
+        } else if ((player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper') ||
+        (player === 'rock' && computer === 'scissors')) {
             playerScore++;
-            return 'Paper beats rock! You win!' + '\n Computer Score: ' + computerScore
-            + '\n Player Score: ' + playerScore;
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerScore++;
-            return 'Scissors beat paper! You lose!' + '\n Computer Score: ' + computerScore
-            + '\n Player Score: ' + playerScore;
-        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            playerScore++;
-            return 'Paper beats rock! You win!' + '\n Computer Score: ' + computerScore
-            + '\n Player Score: ' + playerScore;;
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            playerScore++;
-            return 'Scissors beat paper! You win!' + '\n Computer Score: ' + computerScore
-            + '\n Player Score: ' + playerScore;;
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerScore++;
-            return 'Rock beats scissors! You lose! ' + '\n Computer Score: ' + computerScore
-            + '\n Player Score: ' + playerScore;
-        } else (playerSelection === computerSelection) 
-            return 'It\'s a tie!' + '\n Computer Score: ' + computerScore
-            + '\n Player Score: ' + playerScore;
-        }
-    return playRound();
-}
-
-for (i = 1; i <=5; i++) {
-    console.log(game());
-}
+            return '`${playerFinal}` beats ${computerFinal}`! You win!';
+        } else (player === computer)
+            return 'It\'s a tie!';
+        };
+};
 
 function score() {
     if (playerScore > computerScore) {
-        return 'You won!!!!!!!' + '\n Computer Score: ' + computerScore
-        + '\n Player Score: ' + playerScore;
+        return 'You won!!!!!!!' + '\n Computer Score: ' + computerScore +
+            '\n Player Score: ' + playerScore;
     } else if (playerScore == computerScore) {
-        return 'It\'s a tie!' + '\n Computer Score: ' + computerScore
-        + '\n Player Score: ' + playerScore;
+        return 'It\'s a tie!' + '\n Computer Score: ' + computerScore +
+            '\n Player Score: ' + playerScore;
     } else {
-        return 'You lost!!!!!!!!' + '\n Computer Score: ' + computerScore
-        + '\n Player Score: ' + playerScore;
+        return 'You lost!!!!!!!!' + '\n Computer Score: ' + computerScore +
+            '\n Player Score: ' + playerScore;
     }
 }
 
-console.log(score());
